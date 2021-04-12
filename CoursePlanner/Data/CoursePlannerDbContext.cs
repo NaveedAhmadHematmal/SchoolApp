59fa0457559ceba3c51130b5350bedfdecf78050
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-04-12 15:31:59 +0430

    add: CoursePlanner DbContext

---
+++ CoursePlanner/Data/CoursePlannerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CoursePlanner.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoursePlanner.Data
+{
+    public class CoursePlannerDbContext : DbContext
+    {
+        public CoursePlannerDbContext(DbContextOptions<CoursePlannerDbContext> options) : base(options) { }
+        public DbSet<CourseViewModel> CourseViewModel { get; set; }
+        public DbSet<TeacherViewModel> TeacherViewModel { get; set; }
+        public DbSet<TopicViewModel> TopicViewModel { get; set; }
+    }
+}
